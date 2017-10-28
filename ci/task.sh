0768ba93f5b711a69f399765f4e7dd592b02661a
matthewcosgrove <matthew.cosgrove@emc.com>
2017-10-28 21:27:56 +0100

    Add task script for preparing html report

---
+++ ci/task.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd cucumber-tests || exit
+gradle generateCucumberReport
+cp -r build/reports/cucumber/cucumber-html-reports/* ../cucumber-report
+cd ../cucumber-report || exit
+touch Staticfile
+cat << _EOF_ > index.html
+<!doctype html>
+<html>
+<head>
+    <title>Cucumber Reports</title>
+_EOF_
+
+echo '<meta http-equiv="refresh" content="1; url='${CURRENT_HOST}'/overview-features.html"></head><body></body></html>' >> index.html
+
+ls -ltr
